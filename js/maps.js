39eda88046a252c00c713dc1846a6dca1c55a90d
Ivan Bardon <ivanbardon@gmail.com>
2014-03-27 22:58:22 +0100

    Añadir viras rutas KML i crear el refresco del mapa

--- js/maps.js
+++ js/maps.js
@@ -16,11 +16,14 @@ function inicio() {
   map = new google.maps.Map(document.getElementById('mapa'),mapOptions);
   ruta1 = new google.maps.KmlLayer('https://sites.google.com/site/mapafqu/processo1/Indicaciones%20de%20Punto%202%20a%20Punto%203.kml?attredirects=0&d=1');
   ruta2 = new google.maps.KmlLayer('https://sites.google.com/site/mapafqu/processo1/Indicaciones%20de%20Sortida%20de%20lAjuntament%20a%20Arribada%20a%20lEsgle%CC%81sia.kml?attredirects=0&d=1');
+  ruta3 = new google.maps.KmlLayer('https://sites.google.com/site/mapafqu/processo1/Indicaciones%20de%20Punto%201%20a%20Punto%202.kml?attredirects=0&d=1');
+  ruta4 = new google.maps.KmlLayer('https://sites.google.com/site/mapafqu/processo1/Indicaciones%20de%20Carrer%20Miracle%2C%204%2C%2043550%20Ulldecona%2C%20Tarragona%2C%20Espan%CC%83a%20a%20Avinguda%20de%20la%20Generalitat%2C%2043550%20Ulldecona%2C%20Tarragona%2C%20Espan%CC%83a.kml?attredirects=0&d=1');
+  ruta5 = new google.maps.KmlLayer('https://sites.google.com/site/mapafqu/processo1/Indicaciones%20de%20Carrer%20Guifre%CC%81%2C%2030%2C%2043550%20Ulldecona%2C%20Tarragona%2C%20Espan%CC%83a%20a%20Carrer%20de%20lOlivar%2C%2043550%20Ulldecona%2C%20Tarragona%2C%20Espan%CC%83a.kml?attredirects=0&d=1');
   rutas.push(ruta1);
   rutas.push(ruta2);
-  // rutas.push(ruta3);
-  // rutas.push(ruta4);
-  // rutas.push(ruta5);
+  rutas.push(ruta3);
+  rutas.push(ruta4);
+  rutas.push(ruta5);
 };
 
 google.maps.event.addDomListener(window, 'load', inicio);
