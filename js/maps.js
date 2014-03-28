fe92b7aeefb55dda278aa90a8c6967d271f23e4e
Ivan Bardon <ivanbardon@gmail.com>
2014-03-28 17:58:12 +0100

    Añadir la ventana interactiva

--- js/maps.js
+++ js/maps.js
@@ -5,6 +5,11 @@ var ruta3;
 var ruta4;
 var ruta5;
 var rutas = [];
+var info1 ='<h1>Esta es la ventana</h1><p>que ira cambiando con la información que toque.</p>';
+var info2 ='<h1>Mas ventanas</h1><p>de esas que se muestran solas</p>';
+var info3 ='<h1>Otra window</h1><p>i patatim patatam de que se yo que tecnologia nueva</p>';
+var info4 ='<h1>I hate windows</h1><p>de verdad!</p>';
+var info5 ='<h1>JavaScript lover.</h1><p>i cada vez más.. pro Google.</p>';
 
 function inicio() {
   var mapOptions = {
@@ -34,3 +39,12 @@ function mostrarRutas (ruta) {
   };
   ruta.setMap(map);
 };
+
+function mostrarInfo(info){
+
+  document.getElementById('footer').innerHTML=info;
+
+};
+
+
+
